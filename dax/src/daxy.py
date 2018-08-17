bc05ad8ae311f3bd14fd3ca4455c3622ce14ef78
jclasul <janclaes04@gmail.com>
2018-08-17 17:51:00 +0200

    switch to pandas

--- dax/src/daxy.py
+++ dax/src/daxy.py
@@ -4,6 +4,7 @@ import api_keys
 import requests
 import gdax
 import json
+import pandas as pd
 
 api = api_keys.api_keys()
 
@@ -39,29 +40,38 @@ class GAC(gdax.AuthenticatedClient):
         print(r)
         db.orders.insert_one(r.json())
         return r.json()
+
+    def getorders(self):
+        """{'id': 'e22c9172-0276-47f7-b774-2559784c26aa', 'price': '999.85000000', 
+            'size': '0.01000000', 'product_id': 'BTC-EUR', 'side': 'buy', 
+            'type': 'limit', 'time_in_force': 'GTC', 'post_only': True,
+            'created_at': '2018-08-17T14:57:18.551778Z', 'fill_fees': '0.0000000000000000',
+            'filled_size': '0.00000000', 'executed_value': '0.0000000000000000', 
+            'status': 'open', 'settled': False}"""
     
-    def getbalance(self):
+        self.openorders = client.get_orders()[0]
+        self.df_openorders = pd.DataFrame(self.openorders)
+        print('DAXY DEBUG', self.df_openorders)
+        
+    def getbalances(self):
         """{'id': '459d001f-0391-4e97-89e7-ae474275e2c9', 'currency': 'BTC',
             'balance': '0.0530287336346057', 'available': '0.0530287336346057',
             'hold': '0.0000000000000000', 'profile_id': '5100622b-3ed2-49e4-9810-c28fb96d30b3'} """
 
         self.balances = client.get_accounts()
-        self.balance_BTC = next(item for item in self.balances if item["currency"] == "BTC")
-        self.balance_ETH = next(item for item in self.balances if item["currency"] == "ETH")
-        self.balance_EUR = next(item for item in self.balances if item["currency"] == "EUR")
-        print('EUR balance at {}'.format(self.balance_EUR.get('balance', None)))
-        print('BTC balance at {}'.format(self.balance_BTC.get('balance', None)))
+        self.df_balances = pd.DataFrame(self.balances)
+        print('DAXY DEBUG', self.df_balances)
 
 if __name__ == "__main__":
     client = GAC(key=key,b64secret=b64secret,
                  passphrase=passphrase,
                  api_url="https://api.pro.coinbase.com")
     
-    client.getbalance()
-    counter = 0
+    client.getbalances()
+    client.getorders()
     p_change = {}
 
-    with db.watch() as stream:
+    """with db.watch() as stream:
         for change in stream:
             if counter >= 3 and change.get('fullDocument').get('MONGOKEY', None) == "FBP_UPDATE" and \
                     change.get('fullDocument').get('product_id', None) == "BTC-USD":
@@ -79,7 +89,7 @@ if __name__ == "__main__":
 
                 counter += 1
                 keys = ['_id','y']
-                print('{0}DAXY price at : {1} - {2}'.\
+                print('{0} +DAXY price at : {1} - {2}'.\
                     format(time.ctime(),change.get('fullDocument').get('y',None), counter))
 
-                p_change = change
+                p_change = change"""
