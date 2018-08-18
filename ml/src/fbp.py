474fccf9c26b4550bd4856822bdc5c2787418195
jclasul <janclaes04@gmail.com>
2018-08-18 18:01:40 +0200

    ethereum fbp added

--- ml/src/fbp.py
+++ ml/src/fbp.py
@@ -3,6 +3,7 @@ import numpy as np
 from fbprophet import Prophet
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import random
 import pymongo
 import time
 import api_keys
@@ -13,58 +14,70 @@ client = pymongo.MongoClient(api.mongo)
 # specify the database and collection`
 db = client.gdax.gdaxws
 
-def FBP(db,time_delta=3600*24):  #default value is 5 days
-    df = pd.DataFrame(
-        list(db.find({'MONGOKEY':'MARKET_UPDATE',
-                             'product_id':'BTC-USD',
-                             'timestamp' : {'$gt':time.time()-time_delta}})\
-                        .sort([('timestamp', 1)]))) #sort from ascending
+class ML():
+    def FBP(db,time_delta=3600*24*2):  #default value is 5 days
+        random_ = random.random()
+        if random_ > 0.5:
+            self.product_id = 'BTC-USD'
+        else:
+            self.product_id = 'ETH-USD'
 
-    df['time'] = pd.to_datetime(df['time'],infer_datetime_format=True)    
-    df.set_index('time',drop=True, inplace=True)
-    df = df.resample('min').mean()
-    df['ds'] = df.index.copy(deep=True)
+        df = pd.DataFrame(
+            list(db.find({'MONGOKEY':'MARKET_UPDATE',
+                                'product_id':self.product_id,
+                                'timestamp' : {'$gt':time.time()-time_delta}})\
+                            .sort([('timestamp', 1)]))) #sort from ascending
 
-    m = Prophet(changepoint_prior_scale=0.0002).fit(df)
-    future = m.make_future_dataframe(periods=1, freq='1Min')
-    fcst_0002 = m.predict(future)
-    fcst_0002 = fcst_0002.rename(columns=lambda x: x + "_fcst_0002")
+        df['time'] = pd.to_datetime(df['time'],infer_datetime_format=True)    
+        df.set_index('time',drop=True, inplace=True)
+        df = df.resample('min').mean()
+        df['ds'] = df.index.copy(deep=True)
 
-    m = Prophet(changepoint_prior_scale=0.002).fit(df)
-    future = m.make_future_dataframe(periods=1, freq='1Min')
-    fcst_002 = m.predict(future)
-    fcst_002 = fcst_002.rename(columns=lambda x: x + "_fcst_002")
+        m = Prophet(changepoint_prior_scale=0.0002).fit(df)
+        future = m.make_future_dataframe(periods=1, freq='1Min')
+        fcst_0002 = m.predict(future)
+        fcst_0002 = fcst_0002.rename(columns=lambda x: x + "_fcst_0002")
 
-    keep_columns = ['ds','yhat_lower','yhat_upper','yhat','trend']
-    
-    y_hats_0002 = fcst_0002.iloc[-1][[col + "_fcst_0002" for col in keep_columns]].to_dict()
-    y_hats_002 = fcst_002.iloc[-1][[col + "_fcst_002" for col in keep_columns]].to_dict()
-    return {**y_hats_0002,**y_hats_002}
+        m = Prophet(changepoint_prior_scale=0.002).fit(df)
+        future = m.make_future_dataframe(periods=1, freq='1Min')
+        fcst_002 = m.predict(future)
+        fcst_002 = fcst_002.rename(columns=lambda x: x + "_fcst_002")
 
-def push_mongo(db, y_hats):
-    y_hats.update({'MONGOKEY':'FBP_UPDATE',
-                    'timestamp':time.time(),
-                    '_id':y_hats['ds_fcst_0002'],
-                    'product_id':'BTC-USD'})
-    
-    try:
-        db.insert_one(y_hats)
-        print(y_hats)
-    except Exception:
-        print(time.ctime(),'catching exception parsing mongo')
+        keep_columns = ['ds','yhat_lower','yhat_upper','yhat','trend']
+        
+        y_hats_0002 = fcst_0002.iloc[-1][[col + "_fcst_0002" for col in keep_columns]].to_dict()
+        y_hats_002 = fcst_002.iloc[-1][[col + "_fcst_002" for col in keep_columns]].to_dict()
+        return {**y_hats_0002,**y_hats_002}
+
+    def push_mongo(db, y_hats, current_time):
+        y_hats.update({'MONGOKEY':'FBP_UPDATE',
+                        'timestamp':current_time,
+                        '_id':y_hats['ds_fcst_0002'],
+                        'product_id':self.product_id})
+        
+        try:
+            db.insert_one(y_hats)
+            print(y_hats)
+        except Exception:
+            print(time.ctime(),'catching exception parsing mongo')
 
 if __name__ == "__main__":
-    y_hats = FBP(db)
-    push_mongo(db, y_hats)
     timer = time.time()
-    
+    random_interval = random.randint(10,40)
+
+    FBP = ML().FBP
+    push_mongo = ML().push_mongo
+ 
     while True:
-        if time.time() - timer > 30:
+        current_time = time.time()
+        if current_time - timer > random_interval:
             try:
                 y_hats = FBP(db)
-                push_mongo(db, y_hats)
+                push_mongo(db, y_hats, current_time)
                 timer = time.time()
+                random_interval = random.randint(10,40)
             except:
                 time.sleep(4)
                 timer = time.time()
+                random_interval = random.randint(10,40)
                 
